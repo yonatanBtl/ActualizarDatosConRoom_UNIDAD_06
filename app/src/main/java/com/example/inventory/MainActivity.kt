c02ee701a501311d3531734a999a8e9ddb1aba3e
yonatanBtl <yonatan.bartolo@unmsm.edu.pe>
2023-11-14 23:15:50 -0500

    Initial commit

---
+++ app/src/main/java/com/example/inventory/MainActivity.kt
@@ -0,0 +1,28 @@
+
+package com.example.inventory
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.inventory.ui.theme.InventoryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            InventoryTheme {
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    InventoryApp()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/inventory/ui/navigation/NavigationDestination.kt
@@ -0,0 +1,9 @@
+
+package com.example.inventory.ui.navigation
+
+
+interface NavigationDestination {
+    val route: String
+
+    val titleRes: Int
+}
